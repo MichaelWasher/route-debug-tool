ba58cef08b4a236b7d91bc2c7d88515a42a5e2b4
Michael Washer <mwasher@redhat.com>
2023-01-18 17:27:43 +1000

    Currently working with Route Curl with Service_checks templated. The Route, Service and Pods have allk been calculated from the input

--- main.py
+++ main.py
@@ -3,89 +3,61 @@ import pdb
 import requests
 import time
 from utils import *
+
 # Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
+v1 = client.CoreV1Api()
 
 # Route Specifics
 route_name = "console"
 route_namespace = "openshift-console"
 
 # Dependents variables
-pod_namespace=route_namespace
-service_namespace=route_namespace
-
-v1 = client.CoreV1Api()
+pod_namespace = route_namespace
+service_namespace = route_namespace
 
 # Get the Route definition
 # routes = client.CustomObjectsApi().list_cluster_custom_object(group="route.openshift.io", version="v1", plural="routes")["items"
 
 try:
-    route, status, headers = client.CustomObjectsApi().get_namespaced_custom_object_with_http_info(group="route.openshift.io", version="v1", plural="routes", namespace=route_namespace, name=route_name)
-    
+    route, status, headers = client.CustomObjectsApi().get_namespaced_custom_object_with_http_info(
+        group="route.openshift.io", version="v1", plural="routes", namespace=route_namespace, name=route_name)
+
 except client.exceptions.ApiException as e:
     print("Failed to get route definition. Please ensure the Route name provided and Namespace are correct.")
     print(e)
     exit(1)
-    
+
 # Get backend pods from the Route definition
 # TODO: There can be multiple services defined here. Assuming there is only one defined
 service_selector = route["spec"]["to"]
 if service_selector["kind"] != "Service":
-    print(f"Unknown Route format. `.route.spec.to.kind` value of `{service_selector['kind']}` is unexpected. This should be of type Service")
+    print(
+        f"Unknown Route format. `.route.spec.to.kind` value of `{service_selector['kind']}` is unexpected. This should be of type Service")
     exit(1)
 
 # Find the Service
 service_name = service_selector["name"]
 service = v1.read_namespaced_service(name=service_name, namespace=service_namespace)
-pdb.set_trace()
 
 # Find the associated Pods
 pod_selector = service.spec.selector
-pod_selector_str = ",".join([f"{k}={v}" for k,v in pod_selector.items()])
-pod_list = v1.list_namespaced_pod(namespace=service_namespace,label_selector=pod_selector_str)
+pod_selector_str = ",".join([f"{k}={v}" for k, v in pod_selector.items()])
+pod_list = v1.list_namespaced_pod(namespace=service_namespace, label_selector=pod_selector_str)
 
-# Perform Checks
-# Check the Route with response code checks
-route_domain = route["spec"]["host"]
-print(f"Performing check on {route_domain}")
-r = requests.get(url=route["spec"]["host"], verify=False)
-if 300 > r.status_code >= 200:
-    print(f"Route request succeeded with status code: {r.status_code}")
-else:
-    print(f"The Route request failed with status_code: {r.status_code}")
 
-# Check the Service from inside the cluster:
-service_ip = service.spec.clusterIP
-# TODO: Check these ports against the Route ports defined
-for port in service.spec.ports:
-    r = requests.get(url=f"{service_ip}:{port}", verify=False)
-    if 300 > r.status_code >= 200:
-        print(f"Route request succeeded with status code: {r.status_code}")
-    else:
-        print(f"The Route request failed with status_code: {r.status_code}")
 
 # Create a Namespace for Tests
 debug_namespace = create_debug_namespace()
 
-# Create a debug Pod with Alpine
-debug_pod = create_debug_pod(namespace=debug_namespace.metadata.name)
-
-# Wait for the pod to start
-while True:
-    resp = v1.read_namespaced_pod(name=debug_pod.metadata.name, namespace=debug_namespace.metadata.name)
-    if resp.status.phase != 'Pending':
-        break
-    time.sleep(1)
+# Perform Checks
+check_route(route)
+check_service(service)
+check_pods(pod_list)
 
 # - Check if all Pods are listening on the correct Ports
+for pod in pod_list:
+    listening_ports = get_listening_ports(pod)
+    print(f"Pod '{pod}' is listening to {', '.join(listening_ports)}")
 
 #
-
-# Check if all
-# v1.get_namespaced
-# for i in ret.items:
-#     print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-# Test Cases
-# route_name = "console"
-# route_namespace = "openshift-console"
-# route_name = "console4"
--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ from kubernetes.client import *
 import time
 from kubernetes.stream import stream
 from kubernetes.client.rest import ApiException
+import requests
 
 config.load_kube_config()
 v1 = client.CoreV1Api()
@@ -53,7 +54,7 @@ def create_debug_pod(name="debug-pod", namespace="openshift-console", image="qua
         metadata=metadata,
         spec=pod_spec
     )
-    created_pod= client.CoreV1Api().create_namespaced_pod(namespace, body=pod)
+    created_pod = client.CoreV1Api().create_namespaced_pod(namespace, body=pod)
     print(f"Created Pod: {created_pod.metadata.name}")
     return created_pod
 
@@ -70,7 +71,8 @@ def create_debug_namespace(name):
     namespace
     '''
     # TODO dynamically create the name of the namespace
-    client.CoreV1Api().create_namespace(name= name)
+    client.CoreV1Api().create_namespace(name=name)
+
 
 def wait_for_ready(pod):
     while True:
@@ -78,13 +80,24 @@ def wait_for_ready(pod):
         if resp.status.phase != 'Pending':
             break
         time.sleep(1)
-def get_listening_ports(target_pod):
 
+
+def get_listening_ports(target_pod):
+    """
+
+    Parameters
+    ----------
+    target_pod PodV1
+
+    Returns
+    -------
+    List of Port numbers that are actively listening. If there is an issue, None is returned
+    """
     sniffer_pod = create_debug_pod(node_name=target_pod.spec.node_name)
     # Wait for Pod to be running
     wait_for_ready(sniffer_pod)
 
-    # Run the script to exec netstat
+    # Run the script to output the listening ports (one per line)
     script = f"""
         function get_listening_ports () {{
             NS=$1
@@ -92,10 +105,12 @@ def get_listening_ports(target_pod):
         
             POD_ID=$( crictl pods --namespace=$NS --name=$POD -o json |  jq -r '.items[].id' )
             NS_PATH=$( crictl inspectp $POD_ID | jq -r '.info.runtimeSpec.linux.namespaces[] | select( .type=="network" ) | .path' )
-            nsenter --net=$NS_PATH ss -lptun
+            nsenter --net=$NS_PATH ss -H -lptn
         }}
         get_listening_ports_str=$(declare -f get_listening_ports)
-        chroot /host /bin/sh -c "${{get_listening_ports_str}} ; get_listening_ports {target_pod.metadata.namespace} {target_pod.metadata.name}"
+        func_output=$(chroot /host /bin/sh -c "${{get_listening_ports_str}} ; get_listening_ports {target_pod.metadata.namespace} {target_pod.metadata.name}")
+        # Filter output
+        echo $func_output | cut -d ":" -f 2 | cut -d " " -f 1 | sort -u
     """
 
     # Calling exec and waiting for response
@@ -104,7 +119,6 @@ def get_listening_ports(target_pod):
         '-c', f"{script}"
     ]
     try:
-
         resp = stream(client.api.CoreV1Api().connect_get_namespaced_pod_exec,
                       sniffer_pod.metadata.name,
                       sniffer_pod.metadata.namespace,
@@ -112,6 +126,83 @@ def get_listening_ports(target_pod):
                       stderr=True, stdin=False,
                       stdout=True, tty=False)
         print("Response: " + resp)
-        pdb.set_trace()
+        return resp.splitlines(keepends=False)
     except client.exceptions.ApiException as e:
         print(e)
+        return None
+
+
+def check_route(route):
+    '''
+
+    Parameters
+    ----------
+    route V1Route
+
+    Returns
+    -------
+
+    '''
+    # Check the Route with response code checks
+    route_domain = route["spec"]["host"]
+    print(f"Performing check on {route_domain}")
+    r = requests.get(url="https://" + route["spec"]["host"], verify=False)
+    if 300 > r.status_code >= 200:
+        print(f"Route request succeeded with status code: {r.status_code}")
+    else:
+        print(f"The Route request failed with status_code: {r.status_code}")
+
+
+def check_service(service):
+    """
+
+    Parameters
+    ----------
+    service
+
+    Returns
+    -------
+
+    """
+    # Check the Service from inside the cluster:
+    # TODO: Confirm the port is the same as the route destination
+    service_ip = service.spec.cluster_ip
+    # TODO: Check these ports against the Route ports defined
+    for port in service.spec.ports:
+        port_number = port.port
+        pdb.set_trace()
+        # TODO check this against HTTPS ports
+        r = requests.get(url=f"https://{service_ip}:{port_number}", verify=False)
+        if 300 > r.status_code >= 200:
+            print(f"Service {service.metadata.name} request succeeded with status code: {r.status_code}")
+        else:
+            print(f"Service {service.metadata.name} request failed with status_code: {r.status_code}")
+
+
+def check_pods(pod_list):
+    """
+
+    Parameters
+    ----------
+    pod_list
+
+    Returns
+    -------
+
+    """
+    # TOOD: Should really take in a pod_name as the gatherer pod
+    pass
+
+def get_router_pods():
+    """
+
+    Returns
+    -------
+
+    """
+    # TODO Get the router Pods that are used to host the route
+    # .route.status.routerName
+    # oc get pods -n openshift-ingress-operator
+    #      --selector=ingresscontroller.operator.openshift.io/deployment-ingresscontroller=${router_name}
+
+    # From there we can get perform RSH and assume that Curl and Python are present
